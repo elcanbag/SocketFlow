529056af14315614fc949a21cb8eb9a85ae598ef
elcanbag <elcanbagirov09@gmail.com>
2025-05-01 10:37:34 +0400

    High Level Logs
    Logs save on .log file and also database

---
+++ cubesat/src/main/java/com/example/cubesat/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.example.cubesat.config;
+
+import com.example.cubesat.logging.LoggingInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LoggingInterceptor loggingInterceptor;
+
+    @Autowired
+    public WebConfig(LoggingInterceptor loggingInterceptor) {
+        this.loggingInterceptor = loggingInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor)
+                .addPathPatterns("/**");
+    }
+}
